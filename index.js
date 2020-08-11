92d44287961ddfedfb45fb7b37e8de7ad8768466
Hyptex Off <hyptexoff@gmail.com>
2020-08-11 03:39:17 +0000

    It works, but in webhooks rip

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const WebSocket = require('ws');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const hook = new Discord.WebhookClient('webhook id', 'webhook token');
+
  
-const ws = new WebSocket('ws://RunnyJollySecurity.hyptex.repl.run');
+const wss = new WebSocket.Server({ port: 8080 });
  
-ws.on('open', function open() {
-  ws.send('something');
-});
- 
-ws.on('message', function incoming(data) {
-  console.log(data);
-});
+wss.on('connection', function connection(ws, req) {
+  console.log(req)
+  console.log(ws)
+
+  const ip = req.socket.remoteAddress;
+
+  ws.on('message', function incoming(data) {
+    console.log(data, ip)
+        ws.send(data)
+    hook.send(data);
+
+   
+  });
+        
+       
+    });
+        
+
+   client.login('your token here')    
+
+        
